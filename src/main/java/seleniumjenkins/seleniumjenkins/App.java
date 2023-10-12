34a9dff49fcd78dbf6dbefb6257dcfe3594e253b
unknown <jyothitelukula5@gmail.com>
2023-10-12 13:41:07 +0530

    hey this is jenkins

---
+++ src/main/java/seleniumjenkins/seleniumjenkins/App.java
@@ -0,0 +1,13 @@
+package seleniumjenkins.seleniumjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
